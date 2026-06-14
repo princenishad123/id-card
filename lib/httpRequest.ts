49957aa288315853c9f307000051397525e0fcc6
princenishad123 <pn3147399@gmail.com>
2026-06-14 21:12:15 +0530

    url changed

--- lib/httpRequest.ts
+++ lib/httpRequest.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const httpRequest = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL:"http://api.apnasquad.site",
     withCredentials:true
 })
